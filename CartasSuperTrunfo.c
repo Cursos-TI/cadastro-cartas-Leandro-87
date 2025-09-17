4e28a1ba189a851d11e6e26a03c1a7da333fd282
Leandro Garcia <97965951+Leandro-87@users.noreply.github.com>
2025-09-17 09:33:32 +0000

    Salvar

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,20 +6,45 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  char state;
-  char code[5];
-  char city[30];
-
-
+  char state; char city[30]; int population; int points; double area;  double pib;
 
   // Área para entrada de dados
-  printf("Olá, vamos começar a preencher as cartas do Super Trunfo\n");
+  printf("Olá, vamos começar a preencher as cartas do Super Trunfo!\n");
+  printf("Preencha os campos da sua cidade.\n\n");
 
-  printf("CARTA 1\nQual o estado: ");
+  //CADASTRO DA CARTA 1
+
+  printf("CARTA 1\n");
+  printf("Estado: ");
   scanf("%s", &state);
 
+  printf("Cidade: ");
+  scanf("%s", &city);
+
+  printf("População: ");
+  scanf("%d", &population);
+
+  printf("Área (km2): ");
+  scanf("%.2f", &area);
+
+  printf("PIB: ");
+  scanf("%.2f", &pib);
+
+  printf("Quantidades de pontos turisticos: ");
+  scanf("%d", &points);
+
+  //CADASTRO DA CARTA 2
 
   // Área para exibição dos dados da cidade
+
+  printf("\nCARTA 1\n");
+  printf("Estado: %s\n", state);
+  printf("Código: A01\n");
+  printf("Cidade: %s\n", city);
+  printf("População: %d\n", population);
+  printf("Área: %f\n", area);
+  printf("PIB: %f\n", pib);
+  printf("Pontos Turisticos: %d\n", points);
 
 return 0;
 } 
